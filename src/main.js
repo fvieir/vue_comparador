33e35434cbba04dc10b5c488853d69f82d571908
Fabricio Dourado <fcvieira2@gmail.com>
2022-05-15 18:49:27 -0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import bootstrap from './bootstrap.js'
+
+Vue.config.productionTip = false
+Vue.use(bootstrap)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
